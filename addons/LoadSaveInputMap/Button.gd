cceddf2ac6c0e058155053b88435164d898ef872
endritdev <shabanibros@gmail.com>
2022-09-11 14:35:11 +0200

    Delete addons/LoadSaveInputMap directory

--- addons/LoadSaveInputMap/Button.gd
+++
@@ -1,6 +0,0 @@
-tool
-extends Button
-signal clicked()
-
-func _on_Button_pressed():
-	emit_signal("clicked")
--- addons/LoadSaveInputMap/loadBTN.gd
+++
@@ -1,6 +0,0 @@
-tool
-extends Button
-signal loadSignal
-
-func _on_Button_pressed():
-	emit_signal("loadSignal")
